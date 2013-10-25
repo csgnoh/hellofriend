b42c6ca3763e60a3323ff82b6641219270b61c10
Chee Seng <gitgcs@gmail.com>
2013-10-25 11:28:58 +0800

    Initial Commit

---
+++ HelloFriend/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HelloFriend
+//
+//  Created by Chee Seng on 25/10/13.
+//  Copyright (c) 2013 Chee Seng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
